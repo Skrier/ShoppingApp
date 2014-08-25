7e43855bd238bd57f9782afc99598f12733a94ce
Skrier <alex.lewis79@yahoo.co.uk>
2014-08-25 15:58:42 +0100

    v3

--- js/app.js
+++ js/app.js
@@ -1,55 +1,53 @@
 $(document).ready(function(){
-
-var totalArray = [];
+var totalSummary = 0;
 
 /* clears the list */
 	$('#clearList').on('mousedown', function (event) {
-	        event.preventDefault();
-	        event.stopPropagation();
-	        $('#listVariableExample').children().remove();
+		  event.preventDefault();
+	      $('.listVariableExample').remove();
+	      totalSummary = 0;
+	      totalSummaryVat = 0;
+	      vat =0;
+	      totalArray = [];
+	      $('#listTotal').html(0);
+	      $('#vatTotal').html(0);
+	      $('#shopAppTotal').html(0);
     });
 
 /* adds an item to the list */ 
 	$('#addItem').on('mousedown', function(event) {
-	        event.preventDefault();
-	        event.stopPropagation();
+		    event.preventDefault();
 	        var price = +$('#priceBoxValue').val();
 	        var quantity = +$('#quantityBoxValue').val();
 	        var item = $('#textBoxValue').val();
 	        total = price * quantity;
-	        var appendItem = "<div id='listVariableExample'><span class='contVar1' id='description1'>" + item + "</span><span class='contVar1'>£<span>" + price + "</span></span><span class='contVar1'>" + quantity + "</span><span class='contVar1'>£<span>" + total + "</span></span><span class='contVar1'><input class='button1' type='submit' name='Delete' value='X'></span></div>";       
-	    $('#listVariableExample').hide();
-	    $('#list-container').append(appendItem);
-	    appendItem.slideDown(600);
-	    totalArray.push(total);
-	    summaryTotal();
+	        var appendItem = "<div class='listVariableExample'><span class='contVar1 description1'>" + item + "</span><span class='contVar1'> £<span>" + price + "</span></span><span class='contVar1'>" + quantity + "</span><span class='contVar1'> £<span class='contVarT'>" + total + "</span></span><span class='contVar1'><input class='button1' type='submit' name='Delete' value='X'></span></div>";               
+	        $('#list-container').append(appendItem);
+	        $('#list-container div:last-child').hide().slideDown(500);
+	    totalSummary += total;
+	    summaryTotal();     
+	    $('#priceBoxValue').val(0);
+	    $('#quantityBoxValue').val(0);
+	    $('#textBoxValue').val(0);
     });
 
 /* Updates the summary */ 
    function summaryTotal () {
-        var totalSummary = 0;
-        for (var x = 0; x < totalArray.length; x += 1) {
-        		totalSummary += totalArray[x];
-        };
-        var vat = totalSummary*0.2;
-        var totalSummaryVat = totalSummary + vat;
+        vat = Math.round(totalSummary*0.2*100)/100;
+        totalSummaryVat = totalSummary + vat;
+        $('#listTotal').html(totalSummary);
+        $('#vatTotal').html(vat);
+        $('#shopAppTotal').html(totalSummaryVat);
    }
 
+$('#list-container').on('mousedown', '.button1', function(event) {
+		    event.preventDefault();
+		    var removeItemValue = +$(this).closest('div').find('.contVarT').html();
+		    totalSummary -= removeItemValue;
+		    $(this).closest('div').fadeOut(500);
+		    summaryTotal();
+		    console.log(removeItemValue);
+		    console.log(totalSummary);
+           });		    	                                                                                                       
+
 });
-
-
-
-		    	                                                                                                          
-
- <div class='listVariableExample'>
-		    	<span class='contVar1 description1'>Apples</span>
-		    	<span class='contVar1'>£<span>8</span></span>
-		    	<span class='contVar1'>1</span>
-		    	<span class='contVar1'>£<span>8</span></span>
-		    	<span class='contVar1'><input class='button1' type='submit' name='Delete' value='X'></span>	
-		    </div>			
-
-
-
-
-
